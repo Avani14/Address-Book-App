44e7cc45d4f628ef31dd7e177d4442b5afaef779
Avani14 <trivediavani14@gmail.com>
2022-08-17 13:46:44 +0530

    Added the ability to store in database.

--- src/main/java/com/bridgelabz/addressbookapp/service/AddressBookService.java
+++ src/main/java/com/bridgelabz/addressbookapp/service/AddressBookService.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-//Addition of Service layer
 @Service
 @Slf4j
 public class AddressBookService implements AddressBookServiceInterface{
